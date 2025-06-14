b5ce2ff42491c6a5f7817bfd54e3106e1a1a2f4a
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-06-14 17:56:57 +0630

    add workflows

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket-sta"
+    bucket         = "dev-aman-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
