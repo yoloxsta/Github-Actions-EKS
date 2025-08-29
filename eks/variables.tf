2e9f18ff07bec3df94a28ce8c6ea31a6bb7395c6
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-08-29 14:21:05 +0630

    add ec2

--- eks/variables.tf
+++ eks/variables.tf
@@ -49,3 +49,13 @@ variable "addons" {
     version = string
   }))
 }
+
+variable "ec2_ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "ec2_key_name" {
+  description = "SSH key name to use for the EC2 instance"
+  type        = string
+}
